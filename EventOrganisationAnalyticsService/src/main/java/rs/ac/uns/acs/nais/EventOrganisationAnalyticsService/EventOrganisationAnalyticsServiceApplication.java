162f36a13f0acbef268de8492b625aae2934a99f
Tijana Ostojic <tijanaostojicns@gmail.com>
2026-05-21 17:21:22 +0200

    add EventOrganisationAnalyticsService project skeleton
    pom.xml, Dockerfile, application.yml, main application class and test stub for the Elasticsearch-backed analytics microservice for the event organisation subsystem.

---
+++ EventOrganisationAnalyticsService/src/main/java/rs/ac/uns/acs/nais/EventOrganisationAnalyticsService/EventOrganisationAnalyticsServiceApplication.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.acs.nais.EventOrganisationAnalyticsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrganisationAnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventOrganisationAnalyticsServiceApplication.class, args);
+    }
+}
---
+++ EventOrganisationAnalyticsService/src/test/java/rs/ac/uns/acs/nais/EventOrganisationAnalyticsService/EventOrganisationAnalyticsServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.acs.nais.EventOrganisationAnalyticsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventOrganisationAnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
