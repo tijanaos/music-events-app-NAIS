e7b45adf80e983f0ac85c4c422c1f9ecfb3d4683
Jovana Vlaškalić <vlaskalic.ra93.2022@uns.ac.rs>
2026-05-30 20:21:18 +0200

    Redis

---
+++ AdvertisingSearchService/src/main/java/rs/ac/uns/acs/nais/AdvertisingSearchService/config/RedisConfiguration.java
@@ -0,0 +1,61 @@
+package rs.ac.uns.acs.nais.AdvertisingSearchService.config;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfiguration implements CachingConfigurer {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager() {
+        RedisCacheConfiguration defaultConfig = cacheConfiguration(Duration.ofMinutes(10));
+
+        return RedisCacheManager.builder(redisConnectionFactory())
+                .cacheDefaults(defaultConfig.disableCachingNullValues())
+                .withCacheConfiguration("adTypesAll", cacheConfiguration(Duration.ofMinutes(15)))
+                .withCacheConfiguration("adPhasesAll", cacheConfiguration(Duration.ofMinutes(15)))
+                .build();
+    }
+
+    private RedisCacheConfiguration cacheConfiguration(Duration duration) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(duration)
+                .serializeValuesWith(SerializationPair.fromSerializer(serializer));
+    }
+}
